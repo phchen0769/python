a02fcd90be6edb419ff2b2794b90d8c5a24ba25a
Fedorov <phchen0769@icloud.com>
2025-06-18 16:12:41 +0800

    sina_finace

---
+++ data_articles.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+dividends = [
+    {
+        "stock_type": "A股",
+        "stock_code": "600000",
+        "stock_name": "浦发银行",
+        "announcement_date": "2023-08-30",
+        "ex_dividend_date": "2023-08-30",
+        "dividend_before_tax": "0.5",
+        "payment_date": "2023-08-30",
+    }
+]
